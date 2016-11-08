f5bf55dec81245b0c23115201348f2b4e40b9cb2
tutormetume <tutormetume@gmail.com>
2016-11-07 22:39:10 -0600

    Update main.php

--- main.php
+++ main.php
@@ -7,7 +7,31 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <style>
+    .navbar-brand
+{
+    position: absolute;
+    width: 100%;
+    left: 0;
+    text-align: center;
+    margin:0 auto;
+}
+  </style>
 </head>
 <body>
-<form name="main" action=".php" method="POST">
-  </form> 
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">TutorMe</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="#">About Us</a></li>
+      <li><a href="#">Tutor</a></li> 
+      <li><a href="#">Student</a></li> 
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="#about">Login</a></li>
+    </ul>
+  </div>
+</nav>
