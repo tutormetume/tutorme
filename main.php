d97e6717b5c09d18187ca7c60c6e8bab410ed1a4
tutormetume <tutormetume@gmail.com>
2016-11-07 22:14:19 -0600

    Update and rename main.html to main.php

--- main.php
+++ main.php
@@ -9,10 +9,5 @@
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
-<nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">TutorMe</a>
-    </div>
-  </div>
-</nav>
+<form name="main" action=".php" method="POST">
+  </form> 
